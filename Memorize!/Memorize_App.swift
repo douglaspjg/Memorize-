5eaf729022e15c3075955a05a9c9b52670c5c2e9
Douglas <djp5@williams.edu>
2023-12-29 13:40:41 -0500

    Initial Commit

---
+++ Memorize!/Memorize_App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_App.swift
+//  Memorize!
+//
+//  Created by Douglas Pineda-Gutierrez on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
